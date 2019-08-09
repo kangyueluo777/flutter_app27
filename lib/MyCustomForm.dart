949b427d497832ad5446ae58739810c4ff62165b
koin <kangyueluo777@gmail.com>
2019-08-09 16:27:15 +0800

    Initial commit

---
+++ lib/MyCustomForm.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class MyCustomForm extends StatefulWidget{
+
+  @override
+  _MyCustomFormState createState() => _MyCustomFormState();
+
+}
+
+class _MyCustomFormState extends State<MyCustomForm>{
+  final myController = TextEditingController();
+
+  @override
+  void initState(){
+    super.initState();
+
+    myController.addListener(_printLatestValue);
+  }
+
+  @override
+  void dispose(){
+    myController.dispose();
+    super.dispose();
+  }
+
+  _printLatestValue(){
+    print("Second text field: ${myController.text}");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Retroeve Text Input'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              onChanged: (text){
+                print("First text field: $text");
+              },
+            ),
+            TextField(
+              controller: myController,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+}
